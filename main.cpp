f8e63324c5d0b3e38d606bd691f18291e6297e86
Victor Alves <victor.alves@mymail.champlain.edu>
2017-04-26 21:00:15 -0400

    Fixed Main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include "matthew_schwartzkopf_functions.h"
 #include "nicholas_allen_functions.h"
+#include "victor_alves_functions.h"
 
 using namespace std;
 
@@ -23,7 +24,7 @@ int main()
 	mainMenu(firstName, lastName, startChoice);
 	if (startChoice == 'y' || startChoice == 'Y') {
 		loadCards(cardTop, cardBottom);
-		displayBoard(numPlayerCards, numComputerCards, playerCards, computerCards, playerCardsNum, computerCardsNum, cardTop, cardBottom);
+		displayBoard(numPlayerCards, numComputerCards, playerCardsSuit, computerCardsSuit, playerCardsNum, computerCardsNum, cardTop, cardBottom);
 	}
 	else if (startChoice == 'n' || startChoice == 'N') {
 		cout << "Would you like to exit the program?" << endl;
